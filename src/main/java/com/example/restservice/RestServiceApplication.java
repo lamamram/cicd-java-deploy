bfbebe2eb2c74d03968d401142e8ee590953aff8
mlamamra <mlamamra@dawan.fr>
2023-02-24 14:58:16 +0100

    tomcat ready app !

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,9 +3,14 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class RestServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestServiceApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
